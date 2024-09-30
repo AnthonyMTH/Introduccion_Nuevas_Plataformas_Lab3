8895f1e34f9e1e8f5b1bdcb79086ad49fa01c4f0
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2024-09-30 01:30:44 -0500

    notification with pending intent

--- ejercicios-propuestos/BateryPendingIntentApp/app/src/main/java/com/example/baterypendingintentapp/MainActivity.java
+++ ejercicios-propuestos/BateryPendingIntentApp/app/src/main/java/com/example/baterypendingintentapp/MainActivity.java
@@ -1,24 +1,52 @@
 package com.example.baterypendingintentapp;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private BatteryReceiver batteryReceiver;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView textBatteryStatus = findViewById(R.id.textBatteryStatus);
+        TextView textBatteryLevel = findViewById(R.id.textBatteryLevel);
+        ProgressBar batteryProgressBar = findViewById(R.id.batteryProgressBar);
+
+        batteryReceiver = new BatteryReceiver(textBatteryStatus, textBatteryLevel, batteryProgressBar);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            registerReceiver(batteryReceiver, filter, Context.RECEIVER_EXPORTED);
+        }else{
+            registerReceiver(batteryReceiver, filter);
+        }
+        Log.d(TAG, "BroadcastReceiver registrado con PendingIntent satisfactoriamente.");
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        unregisterReceiver(batteryReceiver);
+        Log.d(TAG, "onPause: BroadcastReceiver desregistrado satisfactoriamente");
     }
 }
